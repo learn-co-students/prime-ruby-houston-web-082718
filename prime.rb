165b092d4324a38e5cd64017cb8cc5c76ab1c352
gwatson86 <gwatson86@gmail.com>
2018-08-15 19:26:03 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(number)
+  possible_factors = []
+  remainders = []
+  x = 2
+  if number <= 1
+    false
+  else
+    while x <= number**0.5
+      possible_factors << x
+      x += 1
+    end
+    possible_factors.each{|n| remainders << number % n}
+    if remainders.include?(0) == true
+      false
+      else
+        true
+    end
+  end
+end
