151b11875f5f82327685218a0103d19b7c6d4ceb
v10let <violet.wmoon@gmail.com>
2018-08-27 19:46:12 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,46 @@
 # Add  code here!
+
+def prime?(number)
+	if number <= 1
+		return false
+	elsif number == 2
+		return true
+	end 		
+	
+	for number_in_loop in 2..(number/2)
+		if number % number_in_loop == 0
+			return false
+		end
+	end
+
+	return true
+end
+
+
+# 	i = 2
+# 	loop do
+# 		if number % i == 0
+# 			false
+# 		end
+
+# 		i++
+
+# 		if i == number
+# 			break
+# 		end
+# 	end
+# 	true
+# end
+
+# def prime?(number)
+# 	if number % 2 == 0
+# 		false
+# 	end
+# 	if number % 3 == 0
+# 		false
+# 	end
+# 	if number % 4 == 0
+# 		false
+# 	end
+# 	true
+# end
